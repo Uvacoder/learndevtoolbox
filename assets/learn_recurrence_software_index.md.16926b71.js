5c261efdae7a141c0115401af7e33e41e5a50ecf
Jordi Colas <jordi.colas@viaplaygroup.com>
2022-10-07 21:24:12 +0200

    New dist version

--- assets/learn_recurrence_software_index.md.16926b71.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c as t}from"./app.a78def92.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"learn/recurrence/software/index.md","lastUpdated":1665147636000}'),a={name:"learn/recurrence/software/index.md"};function n(c,o,s,d,i,p){return r(),t("div")}const l=e(a,[["render",n]]);export{f as __pageData,l as default};
