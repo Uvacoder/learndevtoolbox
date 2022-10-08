fd868006826b79d847488a3d82ee0ac5db5bf98e
Jordi Colas <jordi.colas@viaplaygroup.com>
2022-10-08 11:23:28 +0200

    New dist version

---
+++ assets/index.md.5b6b5eaf.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a,a as r}from"./app.0f9415a3.js";const _=JSON.parse('{"title":"Welcome! \u{1F973}","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1665220989000}'),o={name:"index.md"},n=r("",10),s=[n];function i(d,h,c,l,p,f){return t(),a("div",null,s)}const u=e(o,[["render",i]]);export{_ as __pageData,u as default};
