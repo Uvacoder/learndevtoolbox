a56d9328796fe5ba944df2e03f37811fb52e5413
Jordi Colas <jordi.colas@viaplaygroup.com>
2022-10-08 11:39:44 +0200

    New dist version

--- docs/.vitepress/dist/registerSW.js
+++ docs/.vitepress/dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/developer-toolbox/sw.js', { scope: '/developer-toolbox/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/developer-toolbox/sw.js', { scope: 'developer-toolbox' })})}
